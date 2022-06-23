77fa6aaeb7a181fcd7f22f702d9b07713a04eb0b
gomachuca <jjorgemachuca@gmail.com>
2022-06-23 16:36:28 +0200

    itineracion 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,8 +2,57 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  
+   title: {
+    type: String,
+    required: true, // este elemento es obligatorio, si no, error!
+    unique: true // este elemento no puede repetirse, si se fuese a repetir, error!
+  },
+
+  level: {
+    type: String,
+  enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+
+  ingredients: {
+    type: String,
+     // en caso que la caracteristica no se pase, será el valor indicado.
+  },
+
+  // como agregar arrays como propiedades.
+  cuisine: { 
+      type: String,
+   require: true,
+  },
+
+  dishType: {
+    type: String,
+    enum: ["breakfast","main_course","soup","snack","drink","dessert","other"],
+  },
+
+  image:{
+    type:String,
+    defaultValue: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+ duration:{
+    type:Number, //min="0",
+    
+  },
+ creator:{
+    type:String,
+    
+  },
+created:{
+    type:Date //default:"today",
+    
+  },
+
+
 });
+  
+  
+  // TODO: write the schema
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
